ee60de4776bb7b97b8ad2c1d59aaca5c54863f91
Gowri chandana Gadi <gowrichandana.gadi@gmail.com>
2021-06-18 19:42:17 +0530

    practice bitwise operators

---
+++ bitwise_operators.py
@@ -0,0 +1,25 @@
+#Python program to show
+#Bitwise operators
+a = 10
+b = 4  
+
+#Bitwise Or(|) operator
+print(a|b)
+
+#Bitwise And(&) operator
+print(a&b)
+
+#Bitwise Xor(^) operator
+print(a^b)    
+
+#Bitwise Not(~) operator
+print(~a)
+
+#Shift operators
+#Bitwise right shift operator
+print(a>>1)
+print(b>>1)
+
+#Bitwise left shift operator
+print(a<<1)
+print(b<<1)
