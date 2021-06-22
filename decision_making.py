d1853007fa580b5f871f0bba287d020936360b21
Gowri chandana Gadi <gowrichandana.gadi@gmail.com>
2021-06-22 22:13:38 +0530

    practice decision making

---
+++ decision_making.py
@@ -0,0 +1,27 @@
+#If statement
+N = 99
+if (1<=N and N<=100):
+    print("Yes")
+
+#If-else statement
+N = 3
+if not(N <= 100 and N >= 10):
+    print("Yes")
+else:
+    print("No")
+
+#Nested if Else nested if
+if True :
+    print("One")    
+elif True :
+    print("Two")
+else :
+    print("Three")
+
+#Single statement suits
+var = 100
+if ( var == 100 ) :
+   print ("Value of expression is 100") 
+   print ("Good bye!")    
+
+    
